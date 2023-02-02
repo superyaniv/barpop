2f0adaba7a7baa7c5ca2d5ec31e6eff923ac3689
Yaniv Alfasy <yanivalfasy@gmail.com>
2023-02-02 13:19:16 -0500

    d

---
+++ test.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+console.log(process.env.GMAPS_API_KEY)
+// const busy_hours = require('bar_pop');
+
+// busy_hours(place_id, key).then(data => {
+//     do_something_with(data); 
+//  });
